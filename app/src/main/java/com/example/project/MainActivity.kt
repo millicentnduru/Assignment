6d955c833d0e3cf11621aacf6c7408c478d6ce3c
millicentnduru <millicentnduru@gmail.com>
2022-02-01 17:39:53 +0300

    First Message

---
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.)
+    }
+}
